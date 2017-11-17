0815cdeec5a5824853305d36e1540d072e2949ce
TIgnatchenko <tignatchenko@clarus.tech>
2017-11-17 17:23:26 +0300

    initial3432

--- src/main/java/ru/cedra/landingbot/service/bot/EntityInputUtil.java
+++ src/main/java/ru/cedra/landingbot/service/bot/EntityInputUtil.java
@@ -4,22 +4,22 @@ import ru.cedra.landingbot.anno.Step;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.Arrays;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 public class EntityInputUtil<T> {
 
     public void performInput(Class<T> tClass) {
         List<Field> fields = Arrays.asList(tClass.getFields());
-        Map<Integer, Field> stepFieldMap = fields.stream().map(field -> {
+        Map<Integer, Field> stepFieldMap = new HashMap<>();
+        fields.forEach(field -> {
             List<Annotation> annotations = Arrays.asList(field.getDeclaredAnnotations());
             Optional<Annotation> stepOpt = annotations.stream().filter(a->a.annotationType().equals(Step.class)).findFirst();
             if (stepOpt.isPresent()) {
-                Annotation step = stepOpt.get();
-
+                Step step = (Step) stepOpt.get();
+                stepFieldMap.put(step.value(), field);
             }
         });
+
+
     }
 }
